a491077224220ed3d62375ddc458d7c6d5bd2274
Mikle-tlt <misha.talatynnik.02@gmail.com>
2024-02-08 22:26:39 +0300

    feat: add navigate after login

--- src/components/login/login.js
+++ src/components/login/login.js
@@ -1,13 +1,23 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import {useUserStore} from "../../services/user/user-service";
 import {useNavigate} from "react-router-dom";
 import {toast} from "react-toastify";
 import {options} from "../elements/alert/alert-settings";
 
 const Login = () => {
-
   const { login } = useUserStore();
   const navigate = useNavigate();
+  const user = JSON.parse(localStorage.getItem("user"));
+
+  const getNavigate = () => {
+    if (user && user.role === "ADMIN") {
+      navigate('/profiles')
+    } else if (user && user.role === "MANAGER"){
+      navigate('/clients')
+    }else {
+      navigate('/')
+    }
+  }
 
   const useLogin = async (e) => {
     e.preventDefault()
@@ -17,15 +27,14 @@ const Login = () => {
     }
     await login(userData)
       .then(() => {
-        const user = JSON.parse(localStorage.getItem("user"));
-        if (user.role === "ADMIN") {
-          navigate('/profiles')
-        } else {
-          navigate('/clients')
-        }
+        getNavigate();
       })
       .catch((e) => toast.error(e, options))
   }
+
+  useEffect(() => {
+    getNavigate();
+  }, [])
 
   return (
     <section className="py-5 mt-5">
